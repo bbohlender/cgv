ab5f7589a07d52cdabe1bb2b982e010fc4005bce
Bela Bohlender <bbohlender@coconut-capital.com>
2022-01-11 18:54:57 +0100

    before trying out adding/removing observables

--- src/interprete.ts
+++ src/interprete.ts
@@ -1,8 +1,10 @@
 import {
     combineLatest,
+    EMPTY,
     finalize,
     isObservable,
     map,
+    merge,
     MonoTypeOperatorFunction,
     Observable,
     of,
@@ -16,38 +18,48 @@ import { ParsedEventDefintion, ParsedGrammarDefinition, ParsedStep } from "."
 export type Operation<T> = (values: DeepObservable<T>) => DeepObservable<T>
 
 export type Operations<T> = {
-    [name in string]: Operation<T>
+    [name in string]: Operation<InterpretionValue<T>>
 }
 
-type DeepObservable<T> = Observable<Array<T | DeepObservable<T>>>
+type InterpretionValue<T> = {
+    value: T
+    eventDepthMap: { [identifier in string]?: number }
+}
+
+type DeepObservable<T> = Array<DeepObservable<T>> | Observable<DeepObservable<T>> | T
 
 export function flatten<T>(deep: DeepObservable<T>): Observable<Array<T>> {
-    return deep.pipe(
-        switchMap((values) =>
-            values.length === 0
-                ? of([])
-                : combineLatest(values.map((value) => (isObservable(value) ? flatten(value) : of([value]))))
-        ),
-        map((results) => results.reduce((v1, v2) => v1.concat(v2), []))
-    )
+    if (Array.isArray(deep)) {
+        return deep.length === 0
+            ? of([])
+            : combineLatest(deep.map((value) => flatten(value))).pipe(
+                  map((results) => results.reduce((v1, v2) => v1.concat(v2), []))
+              )
+    } else if (isObservable(deep)) {
+        return deep.pipe(switchMap((result) => flatten(result)))
+    } else {
+        return of([deep])
+    }
 }
 
-function debounceParallel<T>(time: number): MonoTypeOperatorFunction<T> {
+function debounceParallel<T>(time: number, compare: (v1: T, v2: T) => boolean): MonoTypeOperatorFunction<T> {
     return (value) => {
-        const map = new Map<T, number>()
+        const entries: Array<[T, number]> = []
         const subject = new Subject<T>()
-        const cleanup = () => map.forEach((handle) => clearTimeout(handle))
+        const cleanup = () => entries.forEach(([, handle]) => clearTimeout(handle))
         value.subscribe({
             complete: () => {
                 cleanup()
                 subject.complete()
             },
             next: (value) => {
-                let entry = map.get(value)
-                if (entry != null) {
-                    clearTimeout(entry)
+                let entry = entries.find(([e1]) => compare(e1, value))
+                if (entry == null) {
+                    entry = [value, -1]
+                } else {
+                    clearTimeout(entry[1])
                 }
-                map.set(value, setTimeout(() => subject.next(value), time) as any)
+                entry[1] = setTimeout(() => subject.next(value), time) as any
             },
             error: (error) => {
                 cleanup()
@@ -58,23 +70,54 @@ function debounceParallel<T>(time: number): MonoTypeOperatorFunction<T> {
     }
 }
 
+//TODO: caching, premature termination, clone parallel
+
+export function listenFlat<T>(deep: DeepObservable<T>): Observable<{ type: "add" | "remove"; item: T }> {
+    //TODO: make it more efficient by comparing old and new
+    const changeSubject = new Subject<{ type: "add" | "remove"; item: T }>()
+    return merge(listenFlatRec(deep, changeSubject), changeSubject).pipe(finalize(() => changeSubject.complete()))
+}
+
+function listenFlatRec<T>(
+    deep: DeepObservable<T>,
+    changeSubject: Subject<{ type: "add" | "remove"; item: T }>
+): Observable<never> {
+    if (Array.isArray(deep)) {
+        return deep.length === 0 ? EMPTY : merge(...deep.map((value) => listenFlatRec(value, changeSubject)))
+    } else if (isObservable(deep)) {
+        return deep.pipe(switchMap((result) => listenFlatRec(result, changeSubject)))
+    } else {
+        return new Observable(() => {
+            changeSubject.next({ type: "add", item: deep })
+            return () => changeSubject.next({ type: "remove", item: deep })
+        })
+    }
+}
+
 export function interprete<T>(
     input: Observable<Array<T>>,
     grammar: ParsedGrammarDefinition,
     operations: Operations<T>,
-    eventDebounceTime: number = 100,
+    eventDebounceTime: number = 100
 ): Observable<Array<T>> {
     const [firstRule] = Object.values(grammar.rules)
-    const eventMap = new Map<ParsedEventDefintion, Array<[inputs: Array<T>, subscriber: Subscriber<T[]>]>>()
-    const eventScheduler = new Subject<ParsedEventDefintion>()
+    const eventList: Array<
+        [
+            identifier: string,
+            event: ParsedEventDefintion,
+            depth: number,
+            inputs: Array<Array<InterpretionValue<T>>>,
+            subscribers: Array<Subscriber<Array<InterpretionValue<T>>>>
+        ]
+    > = []
+    const eventScheduler = new Subject<[string, ParsedEventDefintion, number]>()
     eventScheduler
         .pipe(
-            debounceParallel(eventDebounceTime),
-            tap((event) => {
-                const entries = eventMap.get(event)
-                if (entries != null && entries.length > 0) {
-                    const results = event(entries.map(([values]) => values))
-                    console.log("event processed", entries.length, results.length)
+            debounceParallel(eventDebounceTime, ([e1, d1], [e2, d2]) => e1 === e2 && d1 === d2),
+            tap(([identifier, event, depth]) => {
+                const entries = eventList.find(([i, e, d]) => i === identifier && e === event && d === depth)
+                if (entries != null && entries[3].length > 0) {
+                    const results = event(entries[3].map((inputs) => inputs.map(({ value }) => value)))
                     if (!Array.isArray(results) || results.length !== entries.length) {
                         throw new Error(
                             `the return value of an event must be an array with the same length as the input array`
@@ -84,7 +127,14 @@ export function interprete<T>(
                         if (!Array.isArray(result)) {
                             throw new Error(`each item in the array returned by the event must be a array be itself`)
                         }
-                        entries[index][1].next(result)
+                        entries[4][index].next(
+                            result.map((value) => ({
+                                value,
+                                eventDepthMap: {
+                                    [identifier]: depth + 1,
+                                },
+                            }))
+                        )
                     })
                 }
             })
@@ -92,52 +142,88 @@ export function interprete<T>(
         .subscribe()
     return flatten(
         interpreteStep(
-            input,
+            input.pipe(
+                map((value) =>
+                    value.map<InterpretionValue<T>>((v) => ({
+                        value: v,
+                        eventDepthMap: {},
+                    }))
+                )
+            ),
             firstRule,
             grammar,
             operations,
-            (identifier, input) =>
+            (identifier, event, input) => {
                 new Observable((subscriber) => {
-                    const event = grammar.events[identifier]
-                    if (event == null) {
-                        subscriber.error(new Error(`unknown event "${identifier}"`))
-                        return
-                    }
-                    let entry = eventMap.get(event)
-                    const newEntryEntry: [Array<T>, Subscriber<Array<T>>] = [input, subscriber]
+                    listenFlat(input).subscribe({
+                        next: ({ type, item }) => {
+                            const depth = item.eventDepthMap[identifier] ?? 0
+                            let entry = eventList.find(
+                                ([i, e, d]) =>
+                                    i === identifier && e === event && d === depth
+                            )
+                            if(entry == null) {
+                                entry = [identifier, event, depth, [], []]
+                                eventList.push(entry)
+                            }
+                            if (type === "add") {
+                                //add
+                                entry[3].push() //add input
+                                entry[4].push() //add subscriber
+                            } else {
+                                //remove subscriber & input
+                            }
+                            eventScheduler.next([identifier, event, item.eventDepthMap[identifier] ?? 0])
+                        },
+                    })
+                    let entry = eventList.get(event)
+                    const newEntryEntry: [Array<InterpretionValue<T>>, Subscriber<Array<InterpretionValue<T>>>] = [
+                        input,
+                        subscriber,
+                    ]
                     if (entry == null) {
                         entry = [newEntryEntry]
-                        eventMap.set(event, entry)
+                        eventList.set(event, entry)
                     } else {
                         entry.push(newEntryEntry)
                     }
-                    eventScheduler.next(event)
-                    console.log("scheduled " + identifier, entry.length)
                     const e = entry
                     return () => {
                         const index = e.findIndex((entryEntry) => entryEntry === newEntryEntry)
                         if (index !== -1) {
                             e.splice(index, 1)
                             if (e.length === 0) {
-                                eventMap.delete(event)
+                                eventList.delete(event)
                             }
                         }
                     }
                 })
+            }
         )
-    ).pipe(finalize(() => eventScheduler.complete()))
+    ).pipe(
+        map((interpretionResults) => interpretionResults.map(({ value }) => value)),
+        finalize(() => eventScheduler.complete())
+    )
 }
 
 export function interpreteStep<T>(
-    input: DeepObservable<T>,
+    input: DeepObservable<InterpretionValue<T>>,
     step: ParsedStep,
     grammar: ParsedGrammarDefinition,
     operations: Operations<T>,
-    scheduleEvent: (identifier: string, input: Array<T>) => Observable<Array<T>>
-): DeepObservable<T> {
+    scheduleEvent: (
+        identifier: string,
+        event: ParsedEventDefintion,
+        input: DeepObservable<InterpretionValue<T>>
+    ) => DeepObservable<InterpretionValue<T>>
+): DeepObservable<InterpretionValue<T>> {
     switch (step.type) {
         case "event":
-            return flatten(input).pipe(switchMap((input) => scheduleEvent(step.identifier, input)))
+            const event = grammar.events[step.identifier]
+            if (event == null) {
+                throw new Error(`unknown event "${step.identifier}"`)
+            }
+            return scheduleEvent(step.identifier, event, input)
         case "operation":
             const operation = operations[step.identifier]
             if (operation == null) {
@@ -145,17 +231,16 @@ export function interpreteStep<T>(
             }
             return operation(interpreteStep(input, step.parameters, grammar, operations, scheduleEvent))
         case "parallel":
-            return of(
-                step.steps.map(
-                    //TODO: clone
-                    (stepOfSteps) => interpreteStep(input, stepOfSteps, grammar, operations, scheduleEvent)
-                )
+            return step.steps.map(
+                //TODO: clone
+                (stepOfSteps) => interpreteStep(input, stepOfSteps, grammar, operations, scheduleEvent)
             )
         case "raw":
-            return of(Array.isArray(step.value) ? step.value : [step.value])
+            return step.value
         case "sequential":
             let current = input
             for (const stepOfSteps of step.steps) {
+                //TODO: premature termination
                 current = interpreteStep(current, stepOfSteps, grammar, operations, scheduleEvent)
             }
             return current
